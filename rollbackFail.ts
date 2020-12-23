9d93ef2b1466e32a84a4da895aefb909bd89ba8c
user1 <62304291+Qrisno@users.noreply.github.com>
2020-12-23 19:53:30 +0400

    fix(store variable type)

--- rollbackFail.ts
+++ rollbackFail.ts
@@ -7,7 +7,7 @@ import {obj} from './main'
             description: 'This action is responsible for reading the most popular customers',
         },
         // callback for main execution
-        call: (store) => {
+        call:async (store) => {
             store++;
             return store;
         },
